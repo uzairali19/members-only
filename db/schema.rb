5013a6170bb73a4b78f57461a48cf5ded0fcd538
Uzair <uzairali19b@gmail.com>
2021-08-18 18:16:36 +0500

    update user functions

--- db/schema.rb
+++ db/schema.rb
@@ -19,7 +19,18 @@ ActiveRecord::Schema.define(version: 2021_08_17_175532) do
     t.integer "user_id"
   end
 
-# Could not dump table "users" because of following StandardError
-#   Unknown type 'sting' for column 'name'
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["name"], name: "index_users_on_name", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
 
 end
